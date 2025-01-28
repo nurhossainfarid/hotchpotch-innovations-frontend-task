1a75c40cab956e6119e48b38304658207c739e6c
nurhossainfarid <faahsan1516@gmail.com>
2025-01-28 11:20:05 +0600

    update servic

--- src/components/service/Service.tsx
+++ src/components/service/Service.tsx
@@ -1,6 +1,8 @@
-import ServiceContent from "./ServiceContent";
 import Result1 from "../../assets/result1.png";
 import Result2 from "../../assets/result2.png";
+import { Button } from "../ui/button";
+import Paragraph from "../ui/paragraph";
+import Title from "../ui/title";
 
 const Service = () => {
   return (
@@ -8,30 +10,44 @@ const Service = () => {
       className="flex flex-col items-center justify-center mx-auto py-10"
       style={{ width: "1200px" }}
     >
-      <ServiceContent
-        oLeft={"1"}
-        oRight={"2"}
-        img={Result1}
-        title={
-          "Create content efficiently and quickly with great AI writing tools"
-        }
-        subtitle={
-          "150k+ users. No Credit Card Required. Pro designs and writing at no cost. Start for free. Ai Writer Tool | Generate text for ecom, social media, website, sales, blogs etc."
-        }
-        btnText={"Start 14 Days Free Trial"}
-      />
-      <ServiceContent
-        oLeft={"2"}
-        oRight={"1"}
-        img={Result2}
-        title={
-          "Write what you want to convey through clear, & authentic writing"
-        }
-        subtitle={
-          "AI-Writer is the most accurate content generation platform and writing tool that helps you transform your text into a completely personalized."
-        }
-        btnText={"Start 14 Days Free Trial"}
-      />
+      <div className="flex flex-col md:flex-row items-center justify-center mx-auto">
+        <div className="w-full">
+          <img src={Result1} className="" alt="" />
+        </div>
+        <div className="w-5/6 flex flex-col items-start justify-center gap-10">
+          <Title
+            text={
+              "Create content efficiently and quickly with great AI writing tools"
+            }
+            fontSize="3xl"
+          />
+          <Paragraph
+            text={
+              "150k+ users. No Credit Card Required. Pro designs and writing at no cost. Start for free. Ai Writer Tool | Generate text for ecom, social media, website, sales, blogs etc."
+            }
+          />
+          <Button className="text-white">Start 14 Days Free Trial</Button>
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row items-center justify-center mx-auto">
+        <div className="w-full order-2">
+          <img src={Result2} className="" alt="" />
+        </div>
+        <div className="w-5/6 flex flex-col items-start justify-center gap-10 order-1">
+          <Title
+            text={
+              "Write what you want to convey through clear, & authentic writing"
+            }
+            fontSize="3xl"
+          />
+          <Paragraph
+            text={
+              "AI-Writer is the most accurate content generation platform and writing tool that helps you transform your text into a completely personalized."
+            }
+          />
+          <Button className="text-white">Start 14 Days Free Trial</Button>
+        </div>
+      </div>
     </div>
   );
 };
